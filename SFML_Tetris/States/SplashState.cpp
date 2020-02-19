24b2cb0be81aa44eb5e5b6b509ed2d7a6072800d
danderle <danderle86@gmail.com>
2020-02-19 22:53:51 +0100

    SplashState Class

---
+++ SFML_Tetris/States/SplashState.cpp
@@ -0,0 +1,38 @@
+#include "SplashState.h"
+#include "MainMenuState.h"
+
+SplashState::SplashState(std::shared_ptr<GameData> _gameData)
+	:
+	gameData(_gameData)
+{
+	test = "Constructed";
+}
+
+void SplashState::Init()
+{
+	gameData->assets.LoadTexture(splashScreenTexture, SPLASH_SCREEN_PATH);
+	splashScreenSprite.setTexture(gameData->assets.GetTexture(splashScreenTexture));
+	float width = splashScreenSprite.getGlobalBounds().width;
+	float height = splashScreenSprite.getGlobalBounds().height;
+	splashScreenSprite.setPosition(WINDOW_WIDTH / 2 - width / 2, WINDOW_HEIGHT / 2 - height / 2);
+}
+
+void SplashState::HandleInput()
+{
+}
+
+void SplashState::Update(float dt)
+{
+	timePassed += dt;
+	if (timePassed > displayTime)
+	{
+		gameData->machine.AddState(std::make_unique<MainMenuState>(gameData));
+	}
+}
+
+void SplashState::Draw()
+{
+	gameData->window.clear();
+	gameData->window.draw(splashScreenSprite);
+	gameData->window.display();
+}
---
+++ SFML_Tetris/States/SplashState.h
@@ -0,0 +1,26 @@
+#pragma once 
+#include "Game.h"
+#include "State.h"
+
+class SplashState : public State
+{
+public:
+	SplashState() = delete;
+	SplashState(std::shared_ptr<GameData> _gameData);
+
+	void Init();
+	void HandleInput();
+	void Update(float dt);
+	void Draw();
+
+private:
+	const std::string splashScreenTexture = "Splash";
+	static constexpr int displayTime = 0;
+
+	float timePassed = 0.f;
+
+	std::shared_ptr<GameData> gameData;
+	std::string  test = "Test";
+	sf::Sprite splashScreenSprite;
+};
+
