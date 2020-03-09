640c8817e1c4049c5eec53d078699b563fbefebd
danderle <danderle86@gmail.com>
2020-03-09 22:38:37 +0100

    added more colors

--- SFML_Tetris/DEFINITIONS.h
+++ SFML_Tetris/DEFINITIONS.h
@@ -9,5 +9,7 @@
 #define ROBOTO_FONT_PATH "Resources\\Roboto-Bold.ttf"
 #define ROBOT_FONT "Roboto"
 
+#define RED sf::Color(255,0,0)
+#define WHITE sf::Color(255,255,255)
 #define BLACK sf::Color(0,0,0)
 #define LIGHTGRAY sf::Color(200,200,200)
