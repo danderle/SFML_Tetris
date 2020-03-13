3882745d80edc0238f152b23978d2a6fe4f6deef
danderle <danderle86@gmail.com>
2020-03-13 19:17:12 +0100

    added green

--- SFML_Tetris/DEFINITIONS.h
+++ SFML_Tetris/DEFINITIONS.h
@@ -10,6 +10,7 @@
 #define ROBOT_FONT "Roboto"
 
 #define RED sf::Color(255,0,0)
+#define GREEN sf::Color(0,255,0)
 #define WHITE sf::Color(255,255,255)
 #define BLACK sf::Color(0,0,0)
 #define LIGHTGRAY sf::Color(200,200,200)
