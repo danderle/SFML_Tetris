b52aa15d477dd0c4c4a70b1492bcc75429d2adb0
danderle <danderle86@gmail.com>
2020-03-21 21:24:38 +0100

    added global definitions

--- SFML_Tetris/DEFINITIONS.h
+++ SFML_Tetris/DEFINITIONS.h
@@ -4,13 +4,21 @@
 #define WINDOW_WIDTH 600
 #define WINDOW_HEIGHT 900
 
+#define TEXTBOX_WIDTH 350
+#define TEXTBOX_HEIGHT 80
+#define CHARACTER_SIZE 40
+#define OUTLINE_THICKNESS -5
+
 #define SPLASH_SCREEN_PATH "Resources\\SplashScreenTetris.png"
 
 #define ROBOTO_FONT_PATH "Resources\\Roboto-Bold.ttf"
 #define ROBOT_FONT "Roboto"
+#define UNISPACE_FONT_PATH "Resources\\Unispace-Rg.ttf"
+#define UNISPACE_FONT "Unispace"
 
 #define RED sf::Color(255,0,0)
 #define GREEN sf::Color(0,255,0)
 #define WHITE sf::Color(255,255,255)
 #define BLACK sf::Color(0,0,0)
 #define LIGHTGRAY sf::Color(200,200,200)
+#define TRANSPARENT sf::Color::Transparent
