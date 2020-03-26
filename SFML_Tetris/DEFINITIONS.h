3ec339a4a159b543c5f5715dbed94aecc4020a46
danderle <danderle86@gmail.com>
2020-03-26 21:11:31 +0100

    added sound files

--- SFML_Tetris/DEFINITIONS.h
+++ SFML_Tetris/DEFINITIONS.h
@@ -3,6 +3,7 @@
 
 #define WINDOW_WIDTH 600
 #define WINDOW_HEIGHT 900
+#define TITLE "Tetris"
 
 #define TEXTBOX_WIDTH 350
 #define TEXTBOX_HEIGHT 80
@@ -15,6 +16,21 @@
 #define ROBOT_FONT "Roboto"
 #define UNISPACE_FONT_PATH "Resources\\Unispace-Rg.ttf"
 #define UNISPACE_FONT "Unispace"
+
+#define TETRIS_SOUND_PATH "Resources\\Sounds\\Tetris.wav"
+#define TETRIS_SOUND "Clear"
+#define FALL_SOUND_PATH "Resources\\Sounds\\Fall.wav"
+#define FALL_SOUND "Fall"
+#define GAMEOVER_SOUND_PATH "Resources\\Sounds\\Gameover.wav"
+#define GAMEOVER_SOUND "Gameover"
+#define LINE_SOUND_PATH "Resources\\Sounds\\Line.wav"
+#define LINE_SOUND "Line"
+#define SELECTION_SOUND_PATH "Resources\\Sounds\\Selection.wav"
+#define SELECTION_SOUND "Selection"
+#define SPLASH_SOUND_PATH "Resources\\Sounds\\SplashScreenSound.wav"
+#define SPLASH_SOUND "SplashScreenSound"
+#define THEME_SOUND_PATH "Resources\\Sounds\\Theme.ogg"
+#define THEME_SOUND "Theme"
 
 #define HIGHSCORE_PATH "HighScores.txt"
 
