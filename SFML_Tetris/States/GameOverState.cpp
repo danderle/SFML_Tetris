1370054ba6ab606a9e7ce132f6f4019be4da9996
danderle <danderle86@gmail.com>
2020-03-30 21:14:16 +0200

    implemented the HighScores class

--- SFML_Tetris/States/GameOverState.cpp
+++ SFML_Tetris/States/GameOverState.cpp
@@ -12,11 +12,15 @@ GameOverState::GameOverState(std::shared_ptr<GameData> _gameData, const unsigned
 
 void GameOverState::Init()
 {
-	LoadHighScores();
-	SetNewHighScore();
+	highScores.Load();
+	highScores.SetNew(newScore);
+	if (highScores.IsNewScore())
+	{
+		newGameBtn.Disable();
+	}
 	auto& font = gameData->assets.GetFont(UNISPACE_FONT);
+	highScores.SetupTop10List(font);
 	SetupTextBoxes(font);
-	SetupHighScoreTexts(font);
 	SetupButtons(font);
 	SetGuiElementPositions();
 }
@@ -29,68 +33,27 @@ void GameOverState::HandleInput()
 
 void GameOverState::HandleInput(const sf::Event& event)
 {
-	if(saveNewScore)
+	if(highScores.IsNewScore())
 	{
-		std::string scoreText = scoreTexts[newScoreIndex].getString();
-		if (!scoreHolderSet && scoreHolder.size() < 3 && event.KeyReleased && event.key.code >= 0 && event.key.code < 27)
+		if (!highScores.IsNewScoreSet() && gameData->input.KeyReleased(event, sf::Keyboard::Backspace))
 		{
-			scoreHolder.push_back(letters[event.key.code]);
-			for (unsigned int i = 0; i < scoreHolder.size(); i++)
-			{
-				scoreText[i] = scoreHolder[i];
-			}
-			if (scoreHolder.size() < 3)
-			{
-				scoreText[scoreHolder.size()] = '_';
-			}
-			scoreTexts[newScoreIndex].setString(scoreText);
+			highScores.RemoveCharFromScoreHolder();
 		}
-		else if (!scoreHolderSet && gameData->input.KeyReleased(event, sf::Keyboard::Backspace))
+		else if (!highScores.IsNewScoreSet() && gameData->input.KeyReleased(event, sf::Keyboard::Enter))
 		{
-			if (scoreHolder.size() != 0)
-			{
-				scoreHolder.pop_back();
-				if (scoreHolder.size() < 3)
-				{
-					scoreText[scoreHolder.size()] = '_';
-					scoreText[scoreHolder.size() + 1] = ' ';
-				}
-				scoreTexts[newScoreIndex].setString(scoreText);
-			}
-		}
-		else if (!scoreHolderSet && gameData->input.KeyReleased(event, sf::Keyboard::Enter))
-		{
-			scoreHolderSet = true;
-			highScores[newScoreIndex].first = scoreHolder;
-			std::ofstream outStream("HighScores.txt");
-			for (auto highScore : highScores)
-			{
-				highScore.first = highScore.first.empty() ? "XXX" : highScore.first;
-				outStream << highScore.first << " " << std::to_string(highScore.second) << std::endl;
-			}
-			outStream.close();
+			highScores.SaveScoreHolder();
 			newGameBtn.Enable();
+		}
+		else if (!highScores.IsNewScoreSet() && event.KeyReleased)
+		{
+			highScores.AddCharToScoreHolder(event.key.code);
 		}
 	}
 }
 
 void GameOverState::Update(float dt)
 {
-	lastFlash += dt;
-	if (saveNewScore && (lastFlash > flashTime))
-	{
-		
-		lastFlash = 0;
-		if (!scoreHolderSet && switchColor)
-		{
-			scoreTexts[newScoreIndex].setFillColor(TRANSPARENT);
-		}
-		else
-		{
-			scoreTexts[newScoreIndex].setFillColor(RED);
-		}
-		switchColor = !switchColor;
-	}
+	highScores.Flash(dt);
 }
 
 void GameOverState::Draw()
@@ -99,11 +62,7 @@ void GameOverState::Draw()
 	gameData->window.clear();
 	newGameBtn.Draw(gameData->window);
 	gameOverTxtBox.Draw(gameData->window);
-	gameData->window.draw(top10Text);
-	for (auto text : scoreTexts)
-	{
-		gameData->window.draw(text);
-	}
+	highScores.Draw(gameData->window);
 	gameData->window.display();
 }
 
@@ -140,87 +99,12 @@ void GameOverState::CheckButtonClick()
 
 void GameOverState::SetGuiElementPositions()
 {
-	gameOverTxtBox.SetPosition({ (WINDOW_WIDTH - TEXTBOX_WIDTH) / 2, TEXTBOX_HEIGHT });
+	highScores.SetGuiElementPositions();
 
 	float padding = 20.f;
-	float yStartPosition = gameOverTxtBox.GetPosition().y + gameOverTxtBox.GetRect().height + padding;
-	top10Text.setPosition((WINDOW_WIDTH - top10Text.getGlobalBounds().width) / 2, yStartPosition);
-	yStartPosition = yStartPosition + top10Text.getCharacterSize() + padding;
-	float xStartPosition = scoreTexts.size() > 0 ? (WINDOW_WIDTH - scoreTexts[0].getLocalBounds().width) / 2 : 0;
-	for (unsigned int i = 0; i < scoreTexts.size(); i++)
-	{
-		float yNextPosition = (i * scoreTexts[i].getCharacterSize()) + padding * (i + 1);
-		scoreTexts[i].setPosition(xStartPosition, yStartPosition + yNextPosition);
-	}
-	yStartPosition = scoreTexts.back().getPosition().y + scoreTexts.back().getLocalBounds().height + padding * 2;
+	float yStartPosition = highScores.GetTopYPosition() - padding - TEXTBOX_HEIGHT;
+	gameOverTxtBox.SetPosition({ (WINDOW_WIDTH - TEXTBOX_WIDTH) / 2, yStartPosition });
 
+	yStartPosition = highScores.GetBottomYPosition() + padding * 2;
 	newGameBtn.SetPosition({ gameOverTxtBox.GetPosition().x, yStartPosition });
-}
-
-void GameOverState::LoadHighScores()
-{
-	std::ifstream inputStream(HIGHSCORE_PATH);
-	std::string line;
-	while (std::getline(inputStream, line).good() && !line.empty())
-	{
-		std::stringstream ss;
-		ss << line;
-		std::string name;
-		unsigned int score = 0;
-		ss >> name;
-		ss >> score;
-		while (name.size() < 3)
-		{
-			name.push_back(' ');
-		}
-		highScores.emplace_back(name, score);
-	}
-	inputStream.close();
-}
-
-void GameOverState::SetNewHighScore()
-{
-	unsigned int index = 0;
-	for (auto& score : highScores)
-	{
-		if (newScore > score.second)
-		{
-			break;
-		}
-		index++;
-	}
-	if (index < maxHighScoresSize)
-	{
-		scoreSet = true;
-		saveNewScore = true;
-		newGameBtn.Disable();
-		auto it = highScores.begin() + index;
-		highScores.insert(it, { "_  ", newScore });
-		newScoreIndex = index;
-	}
-}
-
-void GameOverState::SetupHighScoreTexts(const sf::Font& font)
-{
-	sf::Text text;
-	text.setFont(font);
-	text.setFillColor(RED);
-	unsigned int max = 0;
-	for (unsigned int i = 0; i < 10 && i < highScores.size(); i++)
-	{
-		std::string number = std::to_string(highScores[i].second);
-		max = max < number.size() ? number.size() : max;
-		std::string spacer = "";
-		unsigned int diff = max - number.size();
-		while (spacer.size() <= diff)
-		{
-			spacer.push_back(' ');
-		}
-		std::string setString = highScores[i].first + "                " + spacer + number;
-		text.setString(setString);
-		scoreTexts.push_back(text);
-	}
-	top10Text.setFont(font);
-	top10Text.setFillColor(RED);
-	top10Text.setString("Top 10");
 }
--- SFML_Tetris/States/GameOverState.h
+++ SFML_Tetris/States/GameOverState.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "IGuiElement.h"
 #include "Game.h"
+#include "HighScores.h"
 #include "Button.h"
 #include <fstream>
 
@@ -21,28 +22,12 @@ private:
 	void CheckButtonHover() override;
 	void CheckButtonClick() override;
 	void SetGuiElementPositions() override;
-	void LoadHighScores();
-	void SetNewHighScore();
-	void SetupHighScoreTexts(const sf::Font& font);
 
 private:
 	std::shared_ptr<GameData> gameData;
 	TextBox gameOverTxtBox;
 	Button newGameBtn;
-	sf::Text top10Text;
-	sf::Clock clock;
+	HighScores highScores;
 	unsigned int newScore;
-	bool scoreSet = false;
-	bool scoreHolderSet = false;
-	bool saveNewScore = false;
-	bool switchColor = true;
-	unsigned int newScoreIndex = 0;
-	float lastFlash = 0.f;
-	std::string scoreHolder = "";
-	std::vector<std::pair<std::string, unsigned int>> highScores;
-	std::vector<sf::Text> scoreTexts;
-	static constexpr char letters[27] = { "ABCDEFGHIJKLMNOPQRSTUVWXYZ" };
-	static constexpr float flashTime = 0.3f;
-	static constexpr unsigned int maxHighScoresSize = 10;
 };
 
